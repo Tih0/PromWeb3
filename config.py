5fb8c7b9c4dd91840603d67263b801edba190a27
Tikhon <118998408+Tih0@users.noreply.github.com>
2024-06-13 01:02:24 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 start_delay_min = 10
-start_delay_max = 600
+start_delay_max = 3600
 
 delay_min = 120
-delay_max = 600
+delay_max = 3600
 
 
 low_prom = 10
@@ -11,5 +11,5 @@ high_prom = 12
 useProxy = True
 numberCircles = 1
 shuffle = False
-mode = 'TxYourself'     # Balance, NFT, Token, TxYourself, Random
+mode = 'Random'     # Balance, NFT, Token, TxYourself, Random
 group_accounts = 'MAIN'
